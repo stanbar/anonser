e58e62f341f68815a2fbd87a08194dfca80b998b
Stanislaw Baranski <stan@stan.bar>
2022-11-28 20:19:36 +0100

    update encryption

--- webapp/src/__tests__/crypto.ts
+++ webapp/src/__tests__/crypto.ts
@@ -1,6 +1,11 @@
 import { ec } from 'elliptic'
 import rand from 'randombytes'
-import { encrypt, decrypt } from '../crypto/encrypt'
+import { encrypt, decrypt, deriveKey } from '../crypto/encrypt'
+import { readFileSync } from 'fs'
+import { encrypt as _encrypt, decrypt as _decrypt } from '@futuretense/secret-box'
+import BN from 'bn.js'
+import crypto from 'crypto'
+import CryptoJS, { AES, SHA256, lib, enc } from 'crypto-js';
 
 const secp256k1 = new ec('secp256k1');
 
@@ -12,12 +17,51 @@ it("ECDH works for secp256k1", () => {
         .toEqual(bob.derive(alice.getPublic()).toString(16))
 })
 
+it("ECDH works for secp256k1 compressed", () => {
+    const alice = secp256k1.keyFromPrivate(rand(32))
+    const bob = secp256k1.keyFromPrivate(rand(32))
+
+    const alicePub = alice.getPublic(true, 'hex')
+    const bobPub = bob.getPublic(true, 'hex')
+
+    expect(alice.derive(secp256k1.keyFromPublic(bobPub, 'hex').getPublic()).toString(16))
+        .toEqual(bob.derive(secp256k1.keyFromPublic(alicePub, 'hex').getPublic()).toString(16))
+})
+
 
 it("Encryption works for key derived from ECDH", async () => {
     const alice = secp256k1.keyFromPrivate(rand(32))
     const bob = secp256k1.keyFromPrivate(rand(32))
-    const msg = new Uint8Array([1, 2, 3])
-    const encrypted = await encrypt(msg, alice.getPrivate('hex'), bob.getPublic().encode('hex', false))
-    const decrypted = await decrypt(encrypted, alice.getPrivate('hex'), bob.getPublic().encode('hex', false))
+    const msg = new Uint8Array(Array.from({ length: 100000 }, (v, i) => i % 255))
+    const encrypted = await encrypt(msg, alice.getPrivate('hex'), bob.getPublic().encode('hex', true))
+    const decrypted = await decrypt(encrypted, alice.getPrivate('hex'), bob.getPublic().encode('hex', true))
     expect(decrypted).toEqual(msg)
+})
+
+
+it("Encryption works for a large payload with internal encryption", async () => {
+    const alice = secp256k1.keyFromPrivate(rand(32))
+    const bob = secp256k1.keyFromPrivate(rand(32))
+
+    const file = readFileSync('README.md', null)
+    
+    const msg = file.toString('hex')
+
+    const alicePrivate = alice.getPrivate('hex')
+    const alicePublic = alice.getPublic().encode('hex', true)
+
+
+    const bobPrivate = bob.getPrivate('hex')
+    const bobPublic = bob.getPublic().encode('hex', true)
+
+    const sharedKey1 = deriveKey(alicePrivate, bobPublic);
+    const sharedKey2 = deriveKey(bobPrivate, alicePublic);
+
+    const message = enc.Hex.parse(msg)
+    const options = { iv: enc.Hex.parse("123123"), mode: CryptoJS.mode.CTR, padding: CryptoJS.pad.NoPadding }
+    const encrypted = AES.encrypt(message, enc.Hex.parse(sharedKey1.toString('hex')), options);
+    const decrypted = AES.decrypt(encrypted, enc.Hex.parse(sharedKey2.toString('hex')), options);
+
+
+    expect(enc.Hex.stringify(decrypted)).toEqual(msg)
 })
--- webapp/src/crypto/encrypt.ts
+++ webapp/src/crypto/encrypt.ts
@@ -10,28 +10,36 @@ const normaliseHex = (hex: string): string => {
     return hex
 }
 
-const deriveKey = (privateKey: string, publicKey: string): Buffer => {
+export const deriveKey = (privateKey: string, publicKey: string): Buffer => {
     privateKey = normaliseHex(privateKey)
     publicKey = normaliseHex(publicKey)
 
-    if(Buffer.from(privateKey, 'hex').length !== 20) {
-        console.error('Invalid private key, it should be 20 bytes long', Buffer.from(privateKey, 'hex').length)
+    if(Buffer.from(privateKey, 'hex').length !== 32) {
+        const message = `Invalid private key, it should be 32 bytes long, but found ${Buffer.from(privateKey, 'hex').length} ${privateKey}`
+        console.error(message)
+        throw new Error(message)
     }
     if(Buffer.from(publicKey, 'hex').length !== 33) {
-        console.error('Invalid public key, it should be 33 bytes long', Buffer.from(publicKey, 'hex').length)
+        const message = `Invalid public key, it should be 33 bytes long, but found ${Buffer.from(publicKey, 'hex').length} ${publicKey}`
+        console.error(message)
+        throw new Error(message)
     }
 
     const receiver = secp256k1.keyFromPublic(publicKey, 'hex')
     const derived = secp256k1.keyFromPrivate(privateKey).derive(receiver.getPublic())
-    return Buffer.from(derived.toString('hex'), 'hex')
+    const key = Buffer.from(derived.toString('hex', 32), 'hex')
+    if(key.length !== 32){
+        throw new Error("Derived key is not 32 bytes long")
+    }
+    return key
 }
 
 export const encrypt = async (msg: Uint8Array, privateKey: string, publicKey: string): Promise<Uint8Array> => {
     const key = deriveKey(privateKey, publicKey)
-    return _encrypt(msg, key, null)
+    return _encrypt(msg, key, null, false)
 }
 
 export const decrypt = async (msg: Uint8Array, privateKey: string, publicKey: string): Promise<Uint8Array> => {
     const key = deriveKey(privateKey, publicKey)
-    return _decrypt(msg, key, null)
+    return _decrypt(msg, key, null, false)
 }
