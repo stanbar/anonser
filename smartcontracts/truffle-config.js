54558e6d0ebb915dd99d3da83e1009237b522f79
Stanislaw Baranski <stan@stan.bar>
2022-12-04 14:29:13 +0100

    update ethereum node address

--- smartcontracts/truffle-config.js
+++ smartcontracts/truffle-config.js
@@ -66,7 +66,7 @@ module.exports = {
     // options below to some value.
     //
     development: {
-      host: "anton.gbaranski.com",     // Localhost (default: none)
+      host: "ethereum.anonser.stan.bar",     // Localhost (default: none)
       port: 8545,            // Standard Ethereum port (default: none)
       network_id: "*",       // Any network (default: none)
     },
